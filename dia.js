7716ca435db76e750fc0daed12554bb95ed1d637
raulnetoo <raulalvarezneto@gmail.com>
2023-10-09 14:28:08 -0300

    Dia.js

--- dia.js
+++ dia.js
@@ -12,30 +12,62 @@ function drawDonutChart() {
 
     var data = response.getDataTable();
     
-    var linha0=0
-    var coluna0=0
+    // var linha0=0
+    // var coluna0=0
+    var linhaValor=0
+    var colunaValor=1
+    var linhaDia=0
+    var colunaDia=0
 
-    var valor0=data.getValue(linha0,coluna0)
+
+  var valor=data.getValue(linhaValor,colunaValor)
+  var valorDia=data.getValue(linhaDia,colunaDia)
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'x');
+  data.addColumn('number', 'Producão');
+  data.addColumn({type: 'string', role: 'annotation'});
+
+        data.addRow([valorDia, valor,''+valor]);
+    
+
+    // var valor0=data.getValue(linha0,coluna0)
 
 
     var options = {
-      pieHole: 0.4, // Define o tamanho do buraco no meio do gráfico (0 a 1)
-      pieSize: 10,
-
-      is3D:true,
-      colors: ['#1e2d3b',''],
-      pieSliceBorderColor: '', // Define a cor da borda do gráfico como preto
-      pieSliceTextStyle: {
-        fontSize: 12,
-        color: 'white' // Define a cor do texto das porcentagens como preto
+      // pieHole: 0.4, // Define o tamanho do buraco no meio do gráfico (0 a 1)
+      // pieSize: 10,
+      // pieSliceText: 'value',
+      // is3D:true,
+      colors: ['#1e2d3b'],
+      // pieSliceBorderColor: '', // Define a cor da borda do gráfico como preto
+      // pieSliceTextStyle: {
+      //   fontSize: 12,
+      //   color: 'white' // Define a cor do texto das porcentagens como preto
+      // },
+      hAxis: {
+        minValue: 0,
+        maxValue: 10,
+        ticks: [0, 5, 10],
       },
-      title: 'Gráfico - '+valor0,
-      legend: { position: 'bottom', alignment: 'center' },
-      backgroundColor: 'transparent',
+
+      annotations: {
+        alwaysOutside: true,
+        textStyle: {
+          fontSize:25,
+          bold: true,
+          opacity: 0.8,
+          // auraColor: 'white'
+        },
+      },
+      title: 'Gráfico - '+valorDia,
+      legend: 'none',
+         
+     backgroundColor: 'transparent',
      fontSize: 20,
 
     };
-    var chart = new google.visualization.PieChart(document.getElementById('dia-chart'));
+    var chart = new google.visualization.BarChart(document.getElementById('dia-chart'));
     chart.draw(data, options);
   });
 }
